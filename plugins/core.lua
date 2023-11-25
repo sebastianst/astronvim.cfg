ebdf2d7a8b3c87ea9ef69f965c670ac74f525724
Sebastian Stammler <seb@oplabs.co>
2023-11-25 14:37:06 +0100

    Fix which-key & cutlass interaction

--- plugins/core.lua
+++ plugins/core.lua
@@ -63,15 +63,17 @@ return {
   --   end,
   -- },
   -- By adding to the which-key config and using our helper function you can add more which-key registered bindings
-  -- {
-  --   "folke/which-key.nvim",
-  --   config = function(plugin, opts)
-  --     require "plugins.configs.which-key"(plugin, opts) -- include the default astronvim config that calls the setup call
-  --     -- Add bindings which show up as group name
-  --     local wk = require "which-key"
-  --     wk.register({
-  --       b = { name = "Buffer" },
-  --     }, { mode = "n", prefix = "<leader>" })
-  --   end,
-  -- },
+  {
+    "folke/which-key.nvim",
+    opts = function(_, opts)
+        opts.triggers_blacklist = {
+            -- list of mode / prefixes that should never be hooked by WhichKey
+            -- this is mostly relevant for keymaps that start with a native binding
+            i = { "c", "C", "s", "S", "d", "D", "x", "X", "m", "M", "j", "k" },
+            v = { "c", "C", "s", "S", "d", "D", "x", "X", "m", "M", "j", "k", " ", "g" },
+            n = { "c", "C", "s", "S", "d", "D", "x", "X", "m", "M" },
+        }
+        return opts
+    end,
+  },
 }
