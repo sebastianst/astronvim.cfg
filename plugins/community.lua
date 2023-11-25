d8a2c22c84e328a41fdd77ccb6a902a9a531d23b
Sebastian Stammler <seb@oplabs.co>
2023-11-25 14:38:46 +0100

    Add some astrocommunity plugins

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,6 +4,18 @@ return {
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.motion.nvim-surround" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.editing-support.cutlass-nvim" },
+  {
+    "gbprod/cutlass.nvim",
+    opts = {
+      cut_key = "m",
+      override_del = true,
+    },
+  },
+  { import = "astrocommunity.debugging.nvim-dap-virtual-text" },
+  { import = "astrocommunity.bars-and-lines.smartcolumn-nvim" },
 }
