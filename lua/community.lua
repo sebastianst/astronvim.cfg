13a973887a3eea16baace52b60747cf98c52f883
Sebastian Stammler <seb@oplabs.co>
2024-10-17 18:39:29 +0200

    add avante-nvim from community

--- lua/community.lua
+++ lua/community.lua
@@ -16,6 +16,7 @@ return {
   { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.completion.avante-nvim" },
   { import = "astrocommunity.motion.nvim-surround" },
   { import = "astrocommunity.editing-support.cutlass-nvim" },
   {
