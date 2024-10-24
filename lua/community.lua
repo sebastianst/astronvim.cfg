6b78c212afeda0f6c5f622e15a2c1e68f2c99e5c
Sebastian Stammler <seb@oplabs.co>
2024-10-24 12:31:26 +0200

    add copilot-lua plugin

--- lua/community.lua
+++ lua/community.lua
@@ -15,7 +15,7 @@ return {
   { import = "astrocommunity.pack.bash" },
   { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.colorscheme.catppuccin" },
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.completion.avante-nvim" },
   { import = "astrocommunity.motion.nvim-surround" },
   { import = "astrocommunity.editing-support.cutlass-nvim" },
